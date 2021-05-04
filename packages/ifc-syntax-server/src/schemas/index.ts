f90a62cd65e297a761acca595387574445d2fc17
Alan Rynne <alan@speckle.systems>
2021-05-04 23:06:53 +0200

    Added ifc-syntax-server

---
+++ packages/ifc-syntax-server/src/schemas/index.ts
@@ -0,0 +1,36 @@
+import * as ifc2x3tc1 from "./IFC2X3_TC1.json"
+import * as ifc2x3 from "./IFC2X3.json"
+import * as ifc4add1 from "./IFC4_ADD1.json"
+import * as ifc4add2tc1 from "./IFC4_ADD2_TC1.json"
+import * as ifc4add2 from "./IFC4_ADD2.json"
+import * as ifc4 from "./IFC4.json"
+import * as ifc4x1 from "./IFC4X1.json"
+import * as ifc4x2 from "./IFC4X2.json"
+import * as ifc4x3 from "./IFC4X3.json"
+
+interface IfcVersions {
+  IFC2X3: any
+  IFC2X3_TC1: any
+  IFC4_ADD1: any
+  IFC4_ADD2_TC1: any
+  IFC4_ADD2: any
+  IFC4: any
+  IFC4X1: any
+  IFC4X2: any
+  IFC4X3_RC1: any
+  IFC4X3: any
+}
+const X: IfcVersions = {
+  IFC2X3: ifc2x3,
+  IFC2X3_TC1: ifc2x3tc1,
+  IFC4_ADD1: ifc4add1,
+  IFC4_ADD2_TC1: ifc4add2tc1,
+  IFC4_ADD2: ifc4add2,
+  IFC4: ifc4,
+  IFC4X1: ifc4x1,
+  IFC4X2: ifc4x2,
+  IFC4X3_RC1: ifc4x3,
+  IFC4X3: ifc4x3
+}
+
+export default X
---
+++ packages/ifc-syntax-server/src/settings.ts
@@ -0,0 +1,44 @@
+import {
+  hasConfigurationCapability,
+  globalSettings,
+  documentSettings,
+  connection
+} from "./server"
+
+// The example settings
+export interface IfcSyntaxSettings {
+  diagnostics: {
+    maxNumberOfProblems: number
+  }
+  parser: {
+    maxLineLength: number
+  }
+}
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+export const DefaultSettings: IfcSyntaxSettings = {
+  diagnostics: {
+    maxNumberOfProblems: 1000
+  },
+  parser: {
+    maxLineLength: 1000000
+  }
+}
+
+export function getDocumentSettings(
+  resource: string
+): Thenable<IfcSyntaxSettings> {
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings)
+  }
+  let result = documentSettings.get(resource)
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "ifcSyntax"
+    })
+    documentSettings.set(resource, result)
+  }
+  return result
+}
